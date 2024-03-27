fb77c0d2c172017306d7b86afa69e6d4b8363f4b
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2024-03-27 14:44:47 -0300

    Alteração no diretório Back-End; Inclusão de um script atualizado e alterações na api do banco

--- BackEnd/WebAPI/WebAPI/Controllers/PacientesController.cs
+++ BackEnd/WebAPI/WebAPI/Controllers/PacientesController.cs
@@ -55,13 +55,11 @@ namespace WebAPI.Controllers
             return Ok(pacienteRepository.BuscarPorId(idUsuario));
         }
 
-        [Authorize]
-        [HttpGet("BuscarPorID")]
-        public IActionResult BuscarPorID(Usuario user)
+        //[Authorize]
+        [HttpGet("BuscarPorId")]
+        public IActionResult BuscarPorId(Guid id)
         {
-            Guid idUsuario = user.Id;
-
-            return Ok(pacienteRepository.BuscarPorId(idUsuario));
+            return Ok(pacienteRepository.BuscarPorId(id));
         }
 
         [HttpPost]
@@ -83,13 +81,20 @@ namespace WebAPI.Controllers
 
             user.Paciente.Endereco = new Endereco();
 
-            user.Paciente.Endereco.Cep = pacienteModel.Cep;
-            user.Paciente.Endereco.Numero = pacienteModel.Numero;
             user.Paciente.Endereco.Logradouro = pacienteModel.Logradouro;
+            user.Paciente.Endereco.Numero = pacienteModel.Numero;
+            user.Paciente.Endereco.Cep = pacienteModel.Cep;
+            user.Paciente.Endereco.Cidade = pacienteModel.Cidade;
 
             pacienteRepository.Cadastrar(user);
 
             return Ok();
+        }
+
+        [HttpGet("BuscarPorData")]
+        public IActionResult BuscarPorData(DateTime data, Guid id)
+        {
+            return Ok(pacienteRepository.BuscarPorData(data,id));
         }
     }
 }
