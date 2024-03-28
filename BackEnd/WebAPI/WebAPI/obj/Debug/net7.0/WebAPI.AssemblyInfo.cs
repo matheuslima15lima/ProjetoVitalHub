e4ad4e462286b3a14da9a81de3a4f98069850b53
Matheus Lima <matheus.cavalcanti5@aluno.senai.br>
2024-03-28 17:15:44 -0300

    listagem clinicas

--- BackEnd/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ BackEnd/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8336e3ca6aa3bf13691f592d5a74c90d6f1ba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ed9152e480e01cc780d0551a1bef9fe2533a02")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
