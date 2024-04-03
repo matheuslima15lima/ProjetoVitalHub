5b4abe633dcb4bf309d6a5b17e6c15d5f79ad782
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2024-04-03 17:03:23 -0300

    atualiada a api

--- BackEnd/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ BackEnd/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee32061f3d74589e682eca44544d35f98484f060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e6f0e4cc12877b843ee9e71437ead4d58dec09")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
