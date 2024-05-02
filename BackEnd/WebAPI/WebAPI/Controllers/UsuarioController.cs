8737e0b7d93b9851fddf43ad2d470a129a0fcdc6
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2024-05-02 15:17:52 -0300

    atualização da API

--- BackEnd/WebAPI/WebAPI/Controllers/UsuarioController.cs
+++ BackEnd/WebAPI/WebAPI/Controllers/UsuarioController.cs
@@ -5,6 +5,7 @@ using System.IdentityModel.Tokens.Jwt;
 using WebAPI.Domains;
 using WebAPI.Interfaces;
 using WebAPI.Repositories;
+using WebAPI.Utils.BlobStorage;
 using WebAPI.ViewModels;
 
 namespace WebAPI.Controllers
@@ -20,20 +21,62 @@ namespace WebAPI.Controllers
             usuarioRepository = new UsuarioRepository();
         }
 
-        [Authorize]
         [HttpPut("AlterarSenha")]
-        public IActionResult AlterarSenha(AlterarSenhaViewModel senhas)
+        public IActionResult UpdatePassword(string email, AlterarSenhaViewModel senha)
         {
-            Guid idUsuario = Guid.Parse(HttpContext.User.Claims.First(c => c.Type == JwtRegisteredClaimNames.Jti).Value);
+            try
+            {
+                usuarioRepository.AlterarSenha(email, senha.SenhaNova!);
 
-            bool correto = usuarioRepository.AlterarSenha(idUsuario, senhas.SenhaAntiga, senhas.SenhaNova);
-            if (!correto)
-                return Unauthorized("Senha incorreta");
-
-            return Ok();
+                return Ok("Senha alterada com sucesso !");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        
+        [HttpGet("BuscarPorId")]
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                return Ok(usuarioRepository.BuscarPorId(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
+        [HttpPut("AlterarFotoPerfil")]
+        public async Task<IActionResult> UploadProfileImage(Guid idUsuario, [FromForm] UsuarioViewModel user)
+        {
+            try
+            {
+                Usuario usuarioBuscado = usuarioRepository.BuscarPorId(idUsuario);
+
+                if (usuarioBuscado == null)
+                {
+                    return NotFound("Usuário com o id especificado não foi encontrado");
+                }
+
+                //Lógica para upload de imagem
+                var containerName = "containervitalhubmurilosouza";
+
+                var connectionString = "DefaultEndpointsProtocol=https;AccountName=blobvitalhubmurilosouza;AccountKey=xLTBTBQrWWeGAwbhMuF2dRXRnZODofXuT5+7J5zhoH0hh4hMUkWC8K/0g3nKq9gDa8Fnu02+YhPR+AStWEqzWQ==;EndpointSuffix=core.windows.net";
+
+
+                var fotoUrl = await AzureBlobStorageHelper.UploadImageBlobAsync(user.Arquivo!, connectionString, containerName);
+
+                usuarioRepository.AtualizarFoto(idUsuario, fotoUrl);
+
+                return Ok("Foto de perfil atualizada com sucesso");
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
     }
 }
