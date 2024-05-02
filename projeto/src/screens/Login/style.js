94e3314f75ec5070481e79285ed12874ad6f4d6a
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2024-05-02 15:03:56 -0300

    atualizações gerais e correções gerais

--- projeto/src/screens/Login/style.js
+++
@@ -1,5 +0,0 @@
-import styled, { css } from "styled-components";
-
-export const LoadIcon = styled.ActivityIndicator`
-    ${props => props.animating ? css`display: block` : css`display:none`}
-`
--- projeto/src/screens/PerfilDeUsuario/style.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const BoxCancelPerfil = styled.View`
-    width: 100%;
-    padding-bottom: 60px;
-`
